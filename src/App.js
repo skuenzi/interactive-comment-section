c7e69ff79470b5dd2eb5366b0c65919d9991d238
sara.kuenzi <48669509+skuenzi@users.noreply.github.com>
2022-03-19 19:14:15 -0600

    delete modal created and styled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Footer from './Components/Footer'
 import Comment from './Components/Comment'
+import DeleteModal from './Components/DeleteModal'
+
 
 
 function App() {
   return (
     <div className="App">
-      <Footer />
       <Comment />
+      <DeleteModal />
+      <Footer />
+      
     </div>
   );
 }
